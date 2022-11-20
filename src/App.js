838dc739000230d8dd7312765d5d3ded284fb473
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-11-20 18:44:19 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import { GalleryData } from "./GalleryData";
-import { useEffect, useState } from "react";
+import React from "react";
+import { About, Header, Work, Skills, Footer, Testimonial } from "./container";
+import { Navbar } from "./components";
+import "./App.scss";
 function App() {
-
-  const [data,setData] = useState([]);
-  const [collection,setCollection] = useState([]);
-
-  useEffect(()=>{
-    setData(GalleryData);
-    setCollection([... new Set(GalleryData.map((item)=> item.titile))])
-  },[]) 
-
-  const gallery_filter = (itemData) =>{
-    const filterData = GalleryData.filter((item)=> item.titile == itemData);
-    setData(filterData);
-  }
-
   return (
-    <div className="App">
-      <div className="galleryWrapper">
-        <div className="filterItem">
-          <ul>
-            <li><button onClick={()=> setData(GalleryData)}>All</button></li>
-            {
-              collection.map((item)=> <li><button onClick={()=>{gallery_filter(item)}}>{item}</button></li>)
-            }
-          </ul>
-        </div>
-        <div className="galleryContainer">
-          {
-            data.map((item)=> <div  key={item.id} className="galleryItem"><img src={item.image  } /></div> )
-          }
-        </div>
-      </div>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Footer />
     </div>
   );
 }
