beea46af1efb90fc686963848907624c6410828b
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2023-04-07 16:04:15 +0200

    javascript update

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times');
         $(header).toggleClass('toggle');
     });
-    
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $(header).removeClass('toggle');
+    });
 })
