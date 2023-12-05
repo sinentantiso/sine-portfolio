d1e3ce3538b2f8b1e79ffd7b543b543ae43afb82
Sinethemba Ntantiso <103891865+sinentantiso@users.noreply.github.com>
2023-12-05 17:56:59 +0200

    update

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset = sec.offsetTop - 1;
+        let offset = sec.offsetTop - 150;
     })
     /*==================== sticky navbar ====================*/
 
